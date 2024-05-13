27894526dcca506f057d489e15656732630b2fe1
Nikhil <nikhilrajani309@gmail.com>
2024-05-13 17:39:42 +0530

    Added basic layout of home page and top bar

---
+++ components/shared/Topbar.tsx
@@ -0,0 +1,44 @@
+import { ClerkProvider, OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+
+
+function Topbar(){
+    return (
+        <nav className="topbar">
+            <Link className="flex items-center gap-4" href="/">
+                <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+                <p className="text-heading3-bold text-light-1 max-xs:hidden">KGP Adda</p>
+            </Link>
+
+            <div className="flex items-center gap-1">
+                <ClerkProvider>
+                    <div className="block md:hidden">
+                        <SignedIn>
+                            <SignOutButton>
+                                <div className="flex cursor-pointer">
+                                    <Image
+                                        src="/assets/logout.svg"
+                                        alt="logout"
+                                        height={24}
+                                        width={24}
+                                    />
+                                </div>
+                            </SignOutButton>
+                        </SignedIn>
+                    </div>
+
+                    <OrganizationSwitcher 
+                        appearance={{
+                            elements:{
+                                organizationSwitcherTrigger:"py-2 px-4"
+                            }
+                        }}
+                    />
+                </ClerkProvider>
+            </div>
+        </nav>
+    )
+}
+
+export default Topbar;
